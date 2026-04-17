8d246b8f62ca7f3e931f9b7f222027719d8aa6ae
Matheus Santos <matheushestnaes@gmail.com>
2026-04-17 20:16:25 -0300

    Adicionar arquivos de projeto.

---
+++ Micro-dev-backend- 2 semestre/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Micro_dev_backend__2_semestre.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
