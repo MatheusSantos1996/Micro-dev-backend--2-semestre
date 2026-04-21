1e48c16f510e1406f93ab7bf5df4d2e09547f29e
Matheus Santos <matheushestnaes@gmail.com>
2026-04-20 22:58:21 -0300

    CRUD Veículo

--- Micro-dev-backend- 2 semestre/Models/Veiculo.cs
+++ Micro-dev-backend- 2 semestre/Models/Veiculo.cs
@@ -10,7 +10,7 @@ namespace Micro_dev_backend__2_semestre.Models
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Obrigatório informar o nome")]
+        [Required(ErrorMessage = "Obrigatório informar o nome!")]
 
         public string Nome { get; set; }
 
@@ -19,10 +19,11 @@ namespace Micro_dev_backend__2_semestre.Models
         public string Placa { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o Ano de Fabricação!")]
+        [Display(Name = "Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o Ano do Modelo!")]
-
+        [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
     }
 }
